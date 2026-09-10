53c5436c002d43b86905493b341f735607d1d6f1
AdamJ2009 <adamdj2009@gmail.com>
2026-09-10 15:47:05 +0100

    added deletion of files testing

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,11 @@
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :destroy]
+
   def index
     @documents = Document.all
   end
 
   def show
-    @document = Document.find(params[:id])
   end
 
   def new
@@ -21,7 +22,16 @@ class DocumentsController < ApplicationController
     end
   end
 
+  def destroy
+    @document.destroy
+    redirect_to documents_path, notice: "Document deleted."
+  end
+
   private
+
+  def set_document
+    @document = Document.find(params[:id])
+  end
 
   def document_params
     params.require(:document).permit(:title, :file)
